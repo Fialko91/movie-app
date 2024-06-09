202eb4dc399cfebac87cb8f5f2a4d683a146eb45
Dmytro <dmytrofialko@gmail.com>
2024-06-09 19:17:53 +0300

    2 lesson dz - added new components card-list/card-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
 import {
-  Component, OnInit,
+  Component,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MyComponentComponent} from "./components/my-component/my-component.component";
+import {MovieListComponent} from "./components/movie/movie-list/movie-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MyComponentComponent],
+  imports: [CommonModule, RouterOutlet, MyComponentComponent, MovieListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
   movie = {
-    "backdrop_path": "../assets/img.jpg",
+    "backdrop_path": "../assets/image/img.jpg",
     "id": 653346,
     "overview": "Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
     "release_date": "May 5, 2024",
@@ -28,7 +29,7 @@ export class AppComponent {
   }
 
   handleAddWatchList() {
-    console.log('handleAddFavorite')
+    console.log('handleAddWatchList')
   }
 
 }
